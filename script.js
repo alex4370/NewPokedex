b06753cd6f0c8f193d1cfd43e8da0c675b85f582
alex4370 <alexd4370@gmail.com>
2019-06-04 17:59:58 -0600

    soyinteligente

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Write your code here
+var pokemon = angular.module("Pokemon",[])
+
+pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
+	$scope.Pokebola = [];
+
+
+
+	$http({
+		meth: "GET",
+		url: "https://pokeapi.co/api/v2/Pokemon/"+m
+
+		({})
+	})
+})
